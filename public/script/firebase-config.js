11246f5830e1c341ee29f7b725f8a0e835a70ebd
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-05-31 10:39:10 +0700

    ...

--- public/script/firebase-config.js
+++ public/script/firebase-config.js
@@ -14,3 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export { app };
+
+
+ 
--- public/script/test.js
+++ public/script/test.js
@@ -1,31 +1,53 @@
-const url = "https://script.google.com/macros/s/AKfycbwxQaa_tvaiLL_yk6rOznoonmW2ocf9KZCI5dTfYJ1ZlcTaAheja3kPZqLrKq2GD1zX/exec";
+// const url = "https://script.google.com/macros/s/AKfycbwxQaa_tvaiLL_yk6rOznoonmW2ocf9KZCI5dTfYJ1ZlcTaAheja3kPZqLrKq2GD1zX/exec";
 
-const data = {
-    action: "append",  // Add method field so the server knows what to do
-    requestTime: new Date().getTime(),
-    patientName: "Dac Binh",
-    patientAge: 12,
-    spO2: 75,
-    HR: 120,
-    bodyTemp: 38,
-    wimonMAC: "ASDF:ASDF"
-};
+// const data = {
+//     action: "append",  // Add method field so the server knows what to do
+//     requestTime: new Date().getTime(),
+//     patientName: "Dac Binh",
+//     patientAge: 12,
+//     spO2: 75,
+//     HR: 120,
+//     bodyTemp: 38,
+//     wimonMAC: "ASDF:ASDF"
+// };
 
-async function sendData(data) {
-    try {
-        const response = await fetch(url, {
-            method: "POST",  // Use "POST" instead of "APPEND"
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
+// async function sendData(data) {
+//     try {
+//         const response = await fetch(url, {
+//             method: "POST",  // Use "POST" instead of "APPEND"
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(data)
+//         });
 
-        const result = await response.json();
-        console.log("Response:", result);
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
+//         const result = await response.json();
+//         console.log("Response:", result);
+//     } catch (error) {
+//         console.error("Error:", error);
+//     }
+// }
 
-sendData(data);
+// sendData(data);
+
+
+const url = "https://graph.facebook.com/v22.0/979169530806683/posts"
+
+const GROUP_ID = "979169530806683";
+
+const PAGE_ID = "141152749077588"
+
+const CLONE = "122096927402798091";
+
+const APP_ID = "646189147801670"
+const APP_SECRET = "07071c33867e996809ea80d9cd4c7733"
+
+
+const response = await fetch(url, {
+    method: "GET"
+})
+
+console.log(response);
+const data = await response.json();
+
+console.log(data);
